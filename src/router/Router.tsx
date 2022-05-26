d554dab0013b7d2ad919732c79da0b3ee2032d9c
CreaterSEDre <100061388+CreaterSEDre@users.noreply.github.com>
2022-05-26 23:29:54 +0900

    20220526

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,23 +1,23 @@
 import { memo, VFC } from "react";
-import { match } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 
 import { Home } from "../components/pages/Home";
 import { Login } from "../components/pages/Login";
 import { Setting } from "../components/pages/Setting";
 import { UserManagement } from "../components/pages/UserManagement";
-import { HomeRoutes } from "./HomeRoutes";
 import { Layout } from "./Layout";
+import { NotFound } from "./NotFound";
 
 export const Router: VFC = memo(() => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Layout />}>
+      <Route path="home" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="UserManagement" element={<UserManagement />} />
         <Route path="Setting" element={<Setting />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
       {/* <Route path="/home" render={(match:{url :any}) => (
         <Routes>
           {HomeRoutes.map((route) => (
